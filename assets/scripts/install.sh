d15bceda0a0334cf1a648008b311f235e4100a27
Ashera Cordova <ashera.cordova@gmail.com>
2022-10-05 08:17:32 +0530

    Added play ground sample

---
+++ assets/scripts/install.sh
@@ -0,0 +1,28 @@
+cordova platform add android
+cordova platform add https://github.com/AsheraCordova/cordova-swt.git
+cordova platform add https://github.com/AsheraCordova/cordova-web.git
+
+cordova plugin add https://github.com/AsheraCordova/Core
+cordova plugin add https://github.com/AsheraCordova/CapInsets
+cordova plugin add https://github.com/AsheraCordova/ConstraintLayout
+cordova plugin add https://github.com/AsheraCordova/CSSBorder
+cordova plugin add https://github.com/AsheraCordova/Custom
+cordova plugin add https://github.com/AsheraCordova/DateTime
+cordova plugin add https://github.com/AsheraCordova/Drawer
+cordova plugin add https://github.com/AsheraCordova/GridLayout
+cordova plugin add https://github.com/AsheraCordova/RecycleView
+cordova plugin add https://github.com/AsheraCordova/Toolbar
+
+cordova plugin add https://github.com/AsheraCordova/IQkeyboardManager
+
+cordova plugin add https://github.com/AsheraCordova/ShutterBug
+cordova plugin add https://github.com/AsheraCordova/Glide
+cordova plugin add https://github.com/AsheraCordova/SDWebImagePlugin
+
+cordova plugin add https://github.com/AsheraCordova/Splash
+cordova plugin add https://github.com/AsheraCordova/Dialog
+
+cordova plugin add https://github.com/AsheraCordova/cordova-plugin-camera
+cordova plugin add https://github.com/AsheraCordova/cordova-plugin-camera-swt
+cordova plugin add https://github.com/AsheraCordova/cordova-plugin-qrscanner
+cordova plugin add https://github.com/AsheraCordova/cordova-plugin-qrscanner-swt
