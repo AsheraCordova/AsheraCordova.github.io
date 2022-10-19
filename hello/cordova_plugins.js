8fcdcb5eaf4cfe6c2aa1df49abab170e2c19f3db
Ashera Cordova <ashera.cordova@gmail.com>
2022-10-19 20:01:30 +0530

    Webapp made responsive

--- hello/cordova_plugins.js
+++ hello/cordova_plugins.js
@@ -22,7 +22,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.5",
     "com.ashera.core": "1.0"
   };
 });
