32e977bd0f28d36522b5ba66608df1c1c3b2c958
Ashera Cordova <ashera.cordova@gmail.com>
2025-09-25 16:11:27 +0530

    Added child app sample to Interactive sample

--- interactivesamples/cordova_plugins.js
+++ interactivesamples/cordova_plugins.js
@@ -75,6 +75,267 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "file": "plugins/com.ashera.chip/browser/www/js/add_res.js",
       "pluginId": "com.ashera.chip",
       "runs": true
+    },
+    {
+      "id": "cordova-plugin-file.DirectoryEntry",
+      "file": "plugins/cordova-plugin-file/www/DirectoryEntry.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.DirectoryEntry"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.DirectoryReader",
+      "file": "plugins/cordova-plugin-file/www/DirectoryReader.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.DirectoryReader"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.Entry",
+      "file": "plugins/cordova-plugin-file/www/Entry.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.Entry"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.File",
+      "file": "plugins/cordova-plugin-file/www/File.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.File"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileEntry",
+      "file": "plugins/cordova-plugin-file/www/FileEntry.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileEntry"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileError",
+      "file": "plugins/cordova-plugin-file/www/FileError.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileError"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileReader",
+      "file": "plugins/cordova-plugin-file/www/FileReader.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileReader"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileSystem",
+      "file": "plugins/cordova-plugin-file/www/FileSystem.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileSystem"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileUploadOptions",
+      "file": "plugins/cordova-plugin-file/www/FileUploadOptions.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileUploadOptions"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileUploadResult",
+      "file": "plugins/cordova-plugin-file/www/FileUploadResult.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileUploadResult"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileWriter",
+      "file": "plugins/cordova-plugin-file/www/FileWriter.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileWriter"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.Flags",
+      "file": "plugins/cordova-plugin-file/www/Flags.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.Flags"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.LocalFileSystem",
+      "file": "plugins/cordova-plugin-file/www/LocalFileSystem.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.LocalFileSystem"
+      ],
+      "merges": [
+        "window"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.Metadata",
+      "file": "plugins/cordova-plugin-file/www/Metadata.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.Metadata"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.ProgressEvent",
+      "file": "plugins/cordova-plugin-file/www/ProgressEvent.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.ProgressEvent"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.fileSystems",
+      "file": "plugins/cordova-plugin-file/www/fileSystems.js",
+      "pluginId": "cordova-plugin-file"
+    },
+    {
+      "id": "cordova-plugin-file.requestFileSystem",
+      "file": "plugins/cordova-plugin-file/www/requestFileSystem.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.requestFileSystem"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.resolveLocalFileSystemURI",
+      "file": "plugins/cordova-plugin-file/www/resolveLocalFileSystemURI.js",
+      "pluginId": "cordova-plugin-file",
+      "merges": [
+        "window"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.isChrome",
+      "file": "plugins/cordova-plugin-file/www/browser/isChrome.js",
+      "pluginId": "cordova-plugin-file",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-file.Preparing",
+      "file": "plugins/cordova-plugin-file/www/browser/Preparing.js",
+      "pluginId": "cordova-plugin-file",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-file.browserFileProxy",
+      "file": "plugins/cordova-plugin-file/src/browser/FileProxy.js",
+      "pluginId": "cordova-plugin-file",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-file.fileSystemPaths",
+      "file": "plugins/cordova-plugin-file/www/fileSystemPaths.js",
+      "pluginId": "cordova-plugin-file",
+      "merges": [
+        "cordova"
+      ],
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-file.firefoxFileSystem",
+      "file": "plugins/cordova-plugin-file/www/browser/FileSystem.js",
+      "pluginId": "cordova-plugin-file",
+      "merges": [
+        "window.FileSystem"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.idbFilesystem",
+      "file": "plugins/cordova-plugin-file/www/browser/idb.filesystem.js",
+      "pluginId": "cordova-plugin-file",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-file-transfer.FileTransferError",
+      "file": "plugins/cordova-plugin-file-transfer/www/FileTransferError.js",
+      "pluginId": "cordova-plugin-file-transfer",
+      "clobbers": [
+        "window.FileTransferError"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file-transfer.FileTransfer",
+      "file": "plugins/cordova-plugin-file-transfer/www/FileTransfer.js",
+      "pluginId": "cordova-plugin-file-transfer",
+      "clobbers": [
+        "window.FileTransfer"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file-transfer.BrowserFileTransfer",
+      "file": "plugins/cordova-plugin-file-transfer/www/browser/FileTransfer.js",
+      "pluginId": "cordova-plugin-file-transfer",
+      "clobbers": [
+        "window.FileTransfer"
+      ]
+    },
+    {
+      "id": "cordova-plugin-network-information.network",
+      "file": "plugins/cordova-plugin-network-information/www/network.js",
+      "pluginId": "cordova-plugin-network-information",
+      "clobbers": [
+        "navigator.connection"
+      ]
+    },
+    {
+      "id": "cordova-plugin-network-information.Connection",
+      "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+      "pluginId": "cordova-plugin-network-information",
+      "clobbers": [
+        "Connection"
+      ]
+    },
+    {
+      "id": "cordova-plugin-network-information.NetworkInfoProxy",
+      "file": "plugins/cordova-plugin-network-information/src/browser/network.js",
+      "pluginId": "cordova-plugin-network-information",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-zip.Zip",
+      "file": "plugins/cordova-plugin-zip/zip.js",
+      "pluginId": "cordova-plugin-zip",
+      "clobbers": [
+        "zip"
+      ]
+    },
+    {
+      "id": "cordova-plugin-zip.ZipProxy",
+      "file": "plugins/cordova-plugin-zip/src/browser/ZipProxy.js",
+      "pluginId": "cordova-plugin-zip",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-file-md5.md5chksum",
+      "file": "plugins/cordova-plugin-file-md5/www/md5chksum.js",
+      "pluginId": "cordova-plugin-file-md5",
+      "clobbers": [
+        "md5chksum"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file-downloader.downloader",
+      "file": "plugins/cordova-plugin-file-downloader/src/downloader.js",
+      "pluginId": "cordova-plugin-file-downloader",
+      "clobbers": [
+        "downloader"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -103,6 +364,12 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "com.ashera.navigationview": "1.0",
     "com.ashera.tablayout": "1.0",
     "com.ashera.cardview": "1.0",
-    "com.ashera.chip": "1.0"
+    "com.ashera.chip": "1.0",
+    "cordova-plugin-file": "8.1.4-dev",
+    "cordova-plugin-file-transfer": "2.0.1-dev",
+    "cordova-plugin-network-information": "3.0.1-dev",
+    "cordova-plugin-zip": "3.2.0",
+    "cordova-plugin-file-md5": "0.3.3",
+    "cordova-plugin-file-downloader": "0.4.2"
   };
 });
---
+++ interactivesamples/plugins/cordova-plugin-zip/zip.js
@@ -0,0 +1,46 @@
+cordova.define("cordova-plugin-zip.Zip", function(require, exports, module) {
+var exec = cordova.require('cordova/exec');
+
+function newProgressEvent(result) {
+    var event = {
+        loaded: result.loaded,
+        total: result.total
+    };
+    return event;
+}
+
+/**
+ *  Example:
+ *  const downZipUrl: string = downZipFileEntry.toInternalURL();
+    const downUnzipDirectoryUrl: string = downUnzipDir.toInternalURL();
+    zip.unzip(
+                downZipUrl,
+                downUnzipDirectoryUrl,
+                (result: CordovaZipPluginUnzipResult, errorMessage: string) => {
+                    if (result == CordovaZipPluginUnzipResult.Success) {
+                        resolve();
+                    } else {
+                        this.log.error(errorMessage, 'an error occurred during unzip');
+                        reject('an error occurred during unzip: ' + errorMessage);
+                    }
+                },
+                event => onProgress(event.loaded, event.total));
+ */
+exports.unzip = function (fileNameUrl, outputDirectoryUrl, callback, progressCallback) {
+    var win = function (result) {
+        if (result && typeof result.loaded != "undefined") {
+            if (progressCallback) {
+                return progressCallback(newProgressEvent(result));
+            }
+        } else if (callback) {
+            callback(0);
+        }
+    };
+    var fail = function (result) {
+        if (callback) {
+            callback(-1, result);
+        }
+    };
+    exec(win, fail, 'Zip', 'unzip', [fileNameUrl, outputDirectoryUrl]);
+};
+});
