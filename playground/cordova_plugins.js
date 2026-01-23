e7aecbe54611ac305986588352af7312b8467e34
Ashera Cordova <ashera.cordova@gmail.com>
2026-01-23 10:22:39 +0530

    Updated to include swipe refresh example

--- playground/cordova_plugins.js
+++ playground/cordova_plugins.js
@@ -122,6 +122,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
+    "com.ashera.swiperefreshlayout": "1.0",
     "com.ashera.core": "1.0",
     "com.ashera.capinsets": "1.0",
     "com.ashera.constraintlayout": "1.0",
